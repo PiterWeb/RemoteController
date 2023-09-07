e9aef648f1f383a355ca2349a848ac5352fc6e12
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-09-07 22:49:58 +0200

    try to work between devices

--- src/net/signal.go
+++ src/net/signal.go
@@ -6,43 +6,15 @@
 package net
 
 import (
-	"bufio"
 	"bytes"
 	"compress/gzip"
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"io"
-	"os"
-	"strings"
 )
 
 // Allows compressing offer/answer to bypass terminal input limits.
 const compress = false
-
-// signalMustReadStdin blocks until input is received from stdin
-func signalMustReadStdin() string {
-	r := bufio.NewReader(os.Stdin)
-
-	var in string
-	for {
-		var err error
-		in, err = r.ReadString('\n')
-		if err != io.EOF {
-			if err != nil {
-				panic(err)
-			}
-		}
-		in = strings.TrimSpace(in)
-		if len(in) > 0 {
-			break
-		}
-	}
-
-	fmt.Println("")
-
-	return in
-}
 
 // signalEncode encodes the input in base64
 // It can optionally zip the input before encoding
