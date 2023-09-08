94294283ff85042fa107a8cb172e24015e65ecc6
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-09-08 10:26:37 +0200

    comment removed from main

--- main.go
+++ main.go
@@ -10,29 +10,4 @@ func main() {
 
 	desktop.RunDesktop()
 
-	// gamepads := gamepad.All{}
-
-	// virtualDevice, err := gamepad.GenerateVirtualDevice()
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// defer gamepad.FreeTargetAndDisconnect(virtualDevice)
-
-	// virtualState := new(gamepad.ViGEmState)
-
-	// for range time.Tick(1 * time.Millisecond) {
-	// 	gamepads.Update()
-	// 	for i := range gamepads {
-	// 		pad := &gamepads[i]
-
-	// 		if !pad.Connected {
-	// 			continue
-	// 		}
-
-	// 		go gamepad.UpdateVirtualDevice(virtualDevice, *pad, virtualState)
-
-	// 	}
-	// }
 }
