9b5174829b0b2b4c786a3a83be8a586d5e9b7935
PiterDev <piterzdev@gmail.com>
2024-01-24 18:47:53 +0100

    i18n settup

---
+++ frontend/src/lib/i18n/i18n.ts
@@ -0,0 +1,13 @@
+import { browser } from '$app/environment'
+import { init, register } from 'svelte-i18n'
+
+const defaultLocale = 'en'
+
+register('en', () => import('./en.json'))
+register('es', () => import('./es.json'))
+register('gl', () => import('./gl.json'))
+
+init({
+	fallbackLocale: defaultLocale,
+	initialLocale: browser ? window.navigator.language : defaultLocale,
+})
--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,2 +1,14 @@
-export const prerender = true;
 export const ssr = false;
+export const prerender = true;
+
+import { browser } from '$app/environment'
+import '$lib/i18n/i18n' // Import to initialize. Important :)
+import { locale, waitLocale } from 'svelte-i18n'
+import type { LayoutLoad } from './$types'
+
+export const load: LayoutLoad = async () => {
+	if (browser) {
+		locale.set(window.navigator.language)
+	}
+	await waitLocale()
+}
