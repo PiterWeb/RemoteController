7af152f30d66ad9f12d023c8d8dbd7c80a98cf20
PiterDev <piterzdev@gmail.com>
2024-01-24 18:47:32 +0100

    Error Custom Route

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1 class="text-center text-[clamp(2rem,6vw,4.2rem)] font-black leading-[1.1] xl:text-left">
+	<span
+		class="[&amp;::selection]:text-base-content text-transparent relative col-start-1 row-start-1 bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
+	>
+		{$page.status}
+	</span>
+	-
+	<span
+		class="[&amp;::selection]:text-base-content text-transparent relative col-start-1 row-start-1 bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
+	>
+		{$page.error?.message}
+	</span>
+</h1>
