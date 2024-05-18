153d14f69693ea72fc1d885178f909d099414429
PiterWeb <piterzdev@gmail.com>
2024-05-18 22:19:26 +0200

    expose to frontend plugin utils

--- src/desktop/app.go
+++ src/desktop/app.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 	"strings"
 
+	"github.com/PiterWeb/RemoteController/src/plugins"
 	"github.com/PiterWeb/RemoteController/src/plugins/messaging"
 	"github.com/pion/webrtc/v3"
 )
@@ -89,4 +90,65 @@ func (a *App) TryClosePeerConnection() bool {
 
 func (a *App) GetCurrentOS() string {
 	return strings.ToUpper(runtime.GOOS)
+}
+
+// GetPlugins returns the list of plugins
+func (a *App) GetPlugins() []plugins.Plugin {
+	return plugins.GetPlugins()
+}
+
+// ReloadPlugins will reload the plugins from the json file
+func (a *App) ReloadPlugins() {
+	plugins.ReloadPlugins()
+}
+
+// GetPlugin returns a plugin by name
+func (a *App) GetPlugin(pluginName string) *plugins.Plugin {
+	plugins := plugins.GetPlugins()
+
+	for _, plugin := range plugins {
+		if plugin.Name == pluginName {
+			return &plugin
+		}
+	}
+
+	return nil
+}
+
+// TooglePlugin will toogle a plugin by name
+func (a *App) TooglePlugin(pluginName string) {
+
+	plugins := plugins.GetPlugins()
+
+	for _, plugin := range plugins {
+		if plugin.Name == pluginName {
+			plugin.Toogle()
+			break
+		}
+	}
+
+}
+
+// InitClientPlugin will initialize the client logic of plugin by name
+func (a *App) InitClientPlugin(pluginName string) {
+
+	messaging_port := plugins.MessagingPort.Get()
+
+	// We use the next port for the websocket connection
+	messaging_port_ws := messaging_port + 1
+
+	plugins := plugins.GetPlugins()
+
+	for _, plugin := range plugins {
+		if plugin.Name == pluginName && plugin.IsEnabled() {
+			plugin.Init_client(messaging_port_ws)
+			break
+		}
+	}
+
+}
+
+// ModifyPluginArgs will modify the arguments of a plugin
+func (a *App) ModifyPluginArgs(pluginName string, args []plugins.Plugin_arg, mode string) {
+	plugins.ModifyArgs(pluginName, args, mode)
 }
