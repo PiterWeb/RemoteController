d538d85607778c49b7eba7b85fe45e52e673821a
PiterWeb <piterzdev@gmail.com>
2024-07-27 14:36:54 +0200

    some of the interactive tutorial finished

--- frontend/src/lib/tutorial/driver.ts
+++ frontend/src/lib/tutorial/driver.ts
@@ -1,10 +1,13 @@
 import { driver } from 'driver.js';
 import 'driver.js/dist/driver.css';
 
+// hazer un singletone para el tutorial
+
 const TutorialDriver = driver({
 	animate: true,
     smoothScroll: true,
     stagePadding: 1,
+	stageRadius: 1,
 	onDestroyStarted: () => {
 		if (!TutorialDriver.hasNextStep() || confirm('Are you sure?')) {
 			TutorialDriver.destroy();
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,73 +1,85 @@
-<script>
-
+<script lang="ts">
 	import onwebsite from '$lib/detection/onwebsite';
-	import { _ } from 'svelte-i18n'
+	import { _ } from 'svelte-i18n';
 	import TutorialDriver from '$lib/tutorial/driver';
 	import { goto } from '$app/navigation';
 
-	function startTutorial() {
-		TutorialDriver.setSteps([
-			{
-				element: "#tutorial-config-btn",
-				popover: {
-					title: $_('tutorial_config_title'),
-					description: $_('tutorial_config_description'),
-					onNextClick: () => {
-						goto('/mode/config')
-						setTimeout(() => {
-							TutorialDriver.moveNext()
-						}, 500)
-					},
-				},
-			},
-			{
-				element: "#tutorial-stun-card",
-				popover: {
-					title: $_('tutorial_config_title'),
-					description: $_('tutorial_config_description'),
-					onPrevClick: () => {
-						goto('/')
-						setTimeout(() => {
-							TutorialDriver.movePrevious()
-						}, 500)
-					},
-					onNextClick: () => {
-						goto('/mode/config/advanced/stun')
-						setTimeout(() => {
-							TutorialDriver.moveNext()
-						}, 500)
-					},
-				},
-			},
-			{
-				element: "#tutorial-group-server",
-				popover: {
-					title: $_('tutorial_group_server_title'),
-					description: $_('tutorial_group_server_description'),
-					onPrevClick: () => {
-						goto('/mode/config')
-						setTimeout(() => {
-							TutorialDriver.movePrevious()
-						}, 500)
-					},
-					onNextClick: () => {
-						goto('/mode/config')
-						setTimeout(() => {
-							TutorialDriver.moveNext()
-						}, 500)
-					},
-					
-				}
-			},
-			{
-
-			}
-		])
-
-		TutorialDriver.drive()
+	function goNextTutorial(duration: number = 750) {
+		setTimeout(() => {
+			TutorialDriver.moveNext();
+		}, duration);
 	}
 
+	function goPrevTutorial(duration: number = 750) {
+		setTimeout(() => {
+			TutorialDriver.movePrevious();
+		}, duration);
+	}
 
+	let tutorialSteps = [
+		{
+			element: '#tutorial-config-btn',
+			popover: {
+				title: $_('tutorial_config_title'),
+				description: $_('tutorial_config_description'),
+				onNextClick: () => {
+					goto('/mode/config');
+					goNextTutorial();
+				}
+			}
+		},
+		{
+			element: '#tutorial-language',
+			popover: {
+				title: $_('tutorial_language_title'),
+				description: $_('tutorial_language_description'),
+				onNextClick: () => {
+					tutorialSteps = [...tutorialSteps]
+					TutorialDriver.setSteps(tutorialSteps);
+					goNextTutorial();
+				},
+				onPrevClick: () => {
+					tutorialSteps = [...tutorialSteps]
+					TutorialDriver.setSteps(tutorialSteps);
+					goto('/');
+					goPrevTutorial();
+				}
+			}
+		},
+		{
+			element: '#tutorial-stun-card',
+			popover: {
+				title: $_('tutorial_stun_title'),
+				description: $_('tutorial_stun_description'),
+				onNextClick: () => {
+					goto('/mode/config/advanced/stun');
+					goNextTutorial();
+				}
+			}
+		},
+		{
+			element: '#tutorial-group-server',
+			popover: {
+				title: $_('tutorial_group_server_title'),
+				description: $_('tutorial_group_server_description'),
+				onPrevClick: () => {
+					goto('/mode/config');
+					goPrevTutorial();
+				},
+				onNextClick: () => {
+					goto('/mode/config');
+					goNextTutorial();
+				}
+			}
+		},
+		{}
+	];
+
+	function startTutorial() {
+		TutorialDriver.setSteps(tutorialSteps);
+
+		TutorialDriver.drive();
+	}
 </script>
 
 <button on:click={startTutorial} class="btn btn-primary text-white">
@@ -79,11 +91,11 @@
 		class="[&amp;::selection]:text-base-content text-transparent relative col-start-1 row-start-1 bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
 		>{$_('main_title_choose')}
 	</span>
-	{ $_('main_title_your') }
+	{$_('main_title_your')}
 
 	<span
 		class="[&amp;::selection]:text-base-content text-transparent relative col-start-1 row-start-1 bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-		>{ $_('main_title_role') }</span
+		>{$_('main_title_role')}</span
 	>
 </h2>
 <div class="flex gap-4 mt-4 md:flex-row flex-col">
@@ -92,9 +104,9 @@
 			class="card md:w-96 md:h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 		>
 			<div class="card-body">
-				<h2 class="card-title text-white">{ $_('host_card_title') }</h2>
-				<p class="text-gray-400">{ $_('host_card_description') }</p>
-				<a href="/mode/host" class="btn btn-primary text-white">{ $_('host_card_cta') }</a>
+				<h2 class="card-title text-white">{$_('host_card_title')}</h2>
+				<p class="text-gray-400">{$_('host_card_description')}</p>
+				<a href="/mode/host" class="btn btn-primary text-white">{$_('host_card_cta')}</a>
 			</div>
 		</div>
 	{/if}
@@ -102,9 +114,9 @@
 		class="card md:w-96 md:h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 	>
 		<div class="card-body">
-			<h2 class="card-title text-white">{ $_('client_card_title') }</h2>
-			<p class="text-gray-400">{ $_('client_card_description') }</p>
-			<a href="/mode/client" class="btn btn-primary text-white">{ $_('client_card_cta') }</a>
+			<h2 class="card-title text-white">{$_('client_card_title')}</h2>
+			<p class="text-gray-400">{$_('client_card_description')}</p>
+			<a href="/mode/client" class="btn btn-primary text-white">{$_('client_card_cta')}</a>
 		</div>
 	</div>
 </div>
