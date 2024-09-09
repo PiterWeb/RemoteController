3441cbc1c22a8d8ba9231ab4adea1e5ae660cf66
PiterWeb <piterzdev@gmail.com>
2024-09-09 21:38:20 +0200

    Singelton Tutorial Driver

--- frontend/src/lib/tutorial/driver.ts
+++ frontend/src/lib/tutorial/driver.ts
@@ -1,18 +1,32 @@
 import { driver } from 'driver.js';
+import type { DriveStep, Driver } from 'driver.js';
 import 'driver.js/dist/driver.css';
 
 // hazer un singletone para el tutorial
+let tutorialDriver: Driver;
 
-const TutorialDriver = driver({
-	animate: true,
-    smoothScroll: true,
-    stagePadding: 1,
-	stageRadius: 1,
-	onDestroyStarted: () => {
-		if (!TutorialDriver.hasNextStep() || confirm('Are you sure?')) {
-			TutorialDriver.destroy();
-		}
+function StartTutorial(steps: DriveStep[], currentStep: number = 0) {
+	if (tutorialDriver) {
+		tutorialDriver.destroy();
+		return tutorialDriver;
 	}
-});
 
-export default TutorialDriver;
+	tutorialDriver = driver({
+		animate: true,
+		smoothScroll: true,
+		stagePadding: 1,
+		stageRadius: 1,
+		onDestroyStarted: () => {
+			if (!tutorialDriver.hasNextStep() || confirm('Are you sure?')) {
+				tutorialDriver.destroy();
+			}
+		}
+	});
+
+	tutorialDriver.setSteps(steps);
+	tutorialDriver.drive(currentStep);
+
+	return tutorialDriver;
+}
+
+export default StartTutorial;
