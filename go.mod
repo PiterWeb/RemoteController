ee6232340ed97b941450486e9e9d335b21634202
PiterWeb <piterzdev@gmail.com>
2025-01-26 21:40:56 +0100

    linux code to manage gamepad

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jbdemonte/virtual-device v1.1.0 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
--- src/gamepad/handler_unix.go
+++ src/gamepad/handler_unix.go
@@ -5,8 +5,9 @@ package gamepad
 import (
 	// "fmt"
 
+	"github.com/jbdemonte/virtual-device/gamepad"
 	"github.com/pion/webrtc/v3"
-	// "github.com/pquerna/ffjson/ffjson"
+	"github.com/pquerna/ffjson/ffjson"
 )
 
 func HandleGamepad(gamepadChannel *webrtc.DataChannel) {
@@ -15,19 +16,123 @@ func HandleGamepad(gamepadChannel *webrtc.DataChannel) {
 		return
 	}
 
+	var virtualGamepad *gamepad.VirtualGamepad
+
 	// Create a virtual device
 	gamepadChannel.OnOpen(func() {
 
+		var err error
+
+		virtualGamepad, err = generateVirtualDevice()
+
+		if err != nil {
+			panic(err)
+		}
+
 	})
+
+	defer func() {
+		if err := recover(); err != nil {
+			(*virtualGamepad).Unregister()
+		}
+	}()
+
+	var lastPad GamepadAPIXState
 
 	// Update the virtual device
 	gamepadChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
 
-		// var pad clonedGamepad
+		var actualPad GamepadAPIXState
 
-		// ffjson.Unmarshal(msg.Data, &pad)
+		ffjson.Unmarshal(msg.Data, &actualPad)
 
-		// fmt.Println(pad)
+		go updateVirtualDevice(*virtualGamepad, actualPad, lastPad)
 
 	})
+
+	// Free the virtualGamepad
+	gamepadChannel.OnClose(func() {
+
+		if virtualGamepad == nil {
+			return
+		}
+
+		err := (*virtualGamepad).Unregister()
+
+		if err != nil {
+			panic(err)
+		}
+
+	})
+}
+
+func generateVirtualDevice() (*gamepad.VirtualGamepad, error) {
+
+	g := gamepad.NewXBox360()
+
+	err := g.Register()
+	if err != nil {
+		return nil, err
+	}
+
+	return &g, nil
+
+}
+
+func updateVirtualDevice(virtualGamepad gamepad.VirtualGamepad, actualPad GamepadAPIXState, lastPad GamepadAPIXState) {
+
+	if actualPad.ID != lastPad.ID || actualPad.Index == lastPad.Index || actualPad.Connected != lastPad.Connected {
+		return
+	}
+
+	for i, v := range actualPad.Axes {
+		if actualPad.Axes[i] == lastPad.Axes[i] {
+			continue
+		}
+
+		switch i {
+		case 0:
+			virtualGamepad.MoveLeftStickY(float32(v))
+		case 1:
+			virtualGamepad.MoveLeftStickX(float32(fixLYAxis(v)))
+		case 2:
+			virtualGamepad.MoveRightStickX(float32(v))
+		case 3:
+			virtualGamepad.MoveRightStickY(float32(fixRYAxis(v)))
+		}
+
+	}
+
+	for i := range actualPad.Buttons {
+		if actualPad.Buttons[i].Pressed == lastPad.Buttons[i].Pressed {
+			continue
+		}
+
+		if actualPad.Buttons[i].Pressed {
+			virtualGamepad.Press(buttonAPIXStateToVirtualGamepadButton[i])
+		} else {
+			virtualGamepad.Release(buttonAPIXStateToVirtualGamepadButton[i])
+		}
+
+	}
+
+}
+
+var buttonAPIXStateToVirtualGamepadButton = map[int]gamepad.Button{
+	0:  gamepad.ButtonSouth,
+	1:  gamepad.ButtonEast,
+	2:  gamepad.ButtonWest,
+	3:  gamepad.ButtonNorth,
+	4:  gamepad.ButtonL1,
+	5:  gamepad.ButtonR1,
+	6:  gamepad.ButtonL2,
+	7:  gamepad.ButtonR2,
+	8:  gamepad.ButtonSelect,
+	9:  gamepad.ButtonStart,
+	10: gamepad.ButtonL3,
+	11: gamepad.ButtonR3,
+	12: gamepad.ButtonUp,
+	13: gamepad.ButtonDown,
+	14: gamepad.ButtonLeft,
+	15: gamepad.ButtonRight,
 }
