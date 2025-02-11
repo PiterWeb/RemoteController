ebcb663a039a601f79b1af77393f750a88cbed05
PiterWeb <piterzdev@gmail.com>
2025-02-11 21:17:34 +0100

    added comprobation for frontend running on the exposed service at port 8081

--- frontend/src/lib/detection/onwebsite.ts
+++ frontend/src/lib/detection/onwebsite.ts
@@ -1,4 +1,8 @@
 // This file is used to determine if the app is running on the website(client only) or not.
 const onwebsite = import.meta.env?.VITE_ON_WEBSITE === 'true';
 
+const IS_RUNNING_EXTERNAL = window.location.port === "8081";
+
+export {IS_RUNNING_EXTERNAL}
+
 export default onwebsite;
