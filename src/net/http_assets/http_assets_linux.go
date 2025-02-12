6e994f057fae0eb916dab78d8c9fecca3d19b05c
PiterWeb <piterzdev@gmail.com>
2025-02-12 15:13:59 +0100

    handle routes correctly

--- src/net/http_assets/http_assets_linux.go
+++ src/net/http_assets/http_assets_linux.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/fs"
 	"net/http"
+	"strings"
 )
 
 func InitHTTPAssets(clientPort int, assets embed.FS) error {
@@ -15,7 +16,7 @@ func InitHTTPAssets(clientPort int, assets embed.FS) error {
 		return err
 	}
 
-	http.Handle("/", http.FileServer(http.FS(staticFS)))
+	http.Handle("GET /", FileMiddleware(staticFS, http.FileServer(http.FS(staticFS))))
 
 	err = http.ListenAndServe(fmt.Sprintf(":%d", clientPort), nil)
 
@@ -25,4 +26,28 @@ func InitHTTPAssets(clientPort int, assets embed.FS) error {
 
 	return nil
 
+}
+
+// If .html of the route is available it loads the .html otherwise try to load the given path
+func FileMiddleware(staticFS fs.FS, next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		pathSplited := strings.SplitN(r.URL.Path, "/", 2)
+
+		if len(pathSplited) != 2 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		data, err := fs.ReadFile(staticFS, pathSplited[1]+".html")
+
+		if err != nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		w.WriteHeader(200)
+		w.Write(data)
+	})
 }
