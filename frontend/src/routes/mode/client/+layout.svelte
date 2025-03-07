827995d50e3b9ebcf87a1b907dc83d4d2f81b640
PiterWeb <piterzdev@gmail.com>
2025-03-07 19:47:35 +0100

    new video player implemented

--- frontend/src/routes/mode/client/+layout.svelte
+++ frontend/src/routes/mode/client/+layout.svelte
@@ -1,19 +1,34 @@
-<script>
+<script lang="ts">
+	import media_css from "$lib/css/media-video.css?raw"
+	import 'player.style/microvideo';
 	import { consumingStream } from '$lib/webrtc/stream/stream_signal_hook.svelte';
 
     /** @type {{children?: import('svelte').Snippet}} */
     let {children} = $props()
 
+	let media: HTMLElement | null = $state(null)
+
+	// Apply custom styles to the media element
+	$effect(() => {
+		if (!media) return
+
+		const shadowRoot = media.shadowRoot
+		const styles = document.createElement('style')
+		styles.textContent = media_css
+
+		shadowRoot?.appendChild(styles)
+	})
+
 </script>
 
 {@render children?.()}
 
+<media-theme-microvideo bind:this={media} class:hidden={!consumingStream.value}>
+<!-- svelte-ignore a11y_media_has_caption -->
 <video
+	slot="media"
 	id="stream-video"
-	class="aspect-video"
-	class:hidden={!consumingStream.value}
-	controls
-	controlslist="nodownload"
->
-	<track kind="captions" />
+	playsinline
+	>
 </video>
+</media-theme-microvideo>
