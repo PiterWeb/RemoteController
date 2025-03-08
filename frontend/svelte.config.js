f0b2443c076db2eab29102ef51de3c3af4e8a049
PiterWeb <piterzdev@gmail.com>
2025-03-08 13:05:42 +0100

    second try to use vercel adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,4 @@
-let adapter;
-
-if (import.meta.env?.VITE_ON_WEBSITE === "true") {
-	adapter = (await import('@sveltejs/adapter-vercel')).default
-} else {
-	adapter = (await import('@sveltejs/adapter-static')).default
-}
-
+import adapter from "@sveltejs/adapter-static"
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,7 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
+		adapter: import.meta.env?.VITE_ON_WEBSITE?.VITE_ON_WEBSITE === "true" ? adapter() : adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: "index.html",
